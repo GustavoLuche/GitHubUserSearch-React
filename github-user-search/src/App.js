b714c4ef36ceb90a4199e44f9712839c01944762
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-26 07:40:53 -0300

    Added Search Component

--- github-user-search/src/App.js
+++ github-user-search/src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
+import Container from "react-bootstrap/Container";
+import Search from "./components/Search";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header title="GitHub User Search" />
+      <Container className="App-container">
+        <Search onSearch={null} />
+      </Container>
     </div>
   );
 }
---
+++ github-user-search/src/components/Search.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function Search({ onSearch }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Função para lidar com a pesquisa
+  const handleSearch = () => {
+    if (typeof onSearch === "function" && searchQuery.trim() !== "") {
+      onSearch(searchQuery);
+      setSearchQuery("");
+    } else {
+      console.error("Invalid onSearch function or empty search term.");
+    }
+  };
+
+  // Manipulador para envio de formulário com Enter
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
+    handleSearch(); 
+  };
+
+  return (
+    <div className="App-search">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="form-group">
+          <Form.Control
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <Button
+            variant="outline-light"
+            onClick={handleSearch}
+            disabled={searchQuery.trim() === ""}
+            className="search-button"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-search"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+            </svg>
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  );
+}
+
+export default Search;
