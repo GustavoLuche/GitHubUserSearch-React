357ca5de1c4dc5eda54f53f80cf34d6085b16d7c
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-27 15:43:29 -0300

    Added SpinnerLoading Component

--- github-user-search/src/App.js
+++ github-user-search/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
 import Search from "./components/Search";
+import SpinnerLoading from "./components/SpinnerLoading";
 import ErrorMessage from "./components/ErrorMessage";
 import UserInfo from "./components/UserInfo";
 import UserRepos from "./components/UserRepos";
@@ -13,10 +14,12 @@ function App() {
   const [userData, setUserData] = useState(null);
   const [userRepos, setUserRepos] = useState([]);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   // Função para lidar com a pesquisa de usuário
   const handleSearch = async (username) => {
     try {
+      setIsLoading(true);
       const userDetails = await getUserDetails(username);
       const userRepos = await getUserRepositories(username);
       setUserData(userDetails);
@@ -26,6 +29,8 @@ function App() {
       setUserData(null);
       setUserRepos([]);
       setError("User not found.");
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -34,9 +39,14 @@ function App() {
       <Header title="GitHub User Search" />
       <Container className="App-container">
         <Search onSearch={handleSearch} />
-        {error && <ErrorMessage message={error} />}
-        {userData && <UserInfo userData={userData} />}
-        {userRepos.length > 0 && <UserRepos userRepos={userRepos} />}
+        {isLoading && <SpinnerLoading />}
+        {!isLoading && !error && (
+          <>
+            {userData && <UserInfo userData={userData} />}
+            {userRepos.length > 0 && <UserRepos userRepos={userRepos} />}
+          </>
+        )}
+        {error && !isLoading && <ErrorMessage message={error} />}
       </Container>
     </div>
   );
