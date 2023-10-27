d9d870090b9c249f9090f5f36aa77a709095330e
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-27 19:05:32 -0300

    Enhanced UserInfo Component with Cards

--- github-user-search/src/components/UserRepos.js
+++ github-user-search/src/components/UserRepos.js
@@ -1,21 +1,33 @@
 import React from "react";
-import ListGroup from "react-bootstrap/ListGroup";
+import Card from "react-bootstrap/Card";
 
 const UserRepos = ({ userRepos }) => {
   userRepos.sort((a, b) => new Date(b.updated_at) - new Date(a.updated_at));
 
   return (
     <div>
-      <h2>User Repositories</h2>
-      <ListGroup>
-        {userRepos.map((repo) => (
-          <ListGroup.Item key={repo.id}>
-            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
-              {repo.name}
-            </a>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+      <h2 className="text-uppercase text-center" style={{ color: '#fff' }}>Repositories</h2>
+      {userRepos.map((repo) => (
+        <Card key={repo.id} className="mb-3">
+          <Card.Body>
+            <Card.Title>
+              <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                {repo.name}
+              </a>
+            </Card.Title>
+            {repo.description && <Card.Text>{repo.description}</Card.Text>}
+            {repo.language && (
+              <Card.Text>
+                <strong>Language:</strong> {repo.language}
+              </Card.Text>
+            )}
+            <Card.Text>
+              <strong>Last Updated:</strong>{" "}
+              {new Date(repo.updated_at).toLocaleDateString()}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      ))}
     </div>
   );
 };
