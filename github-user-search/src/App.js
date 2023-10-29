e6e1acd6ff0338d822f5d7ce80550f63a1ff4cad
Gustavo Luche <92736531+GustavoLuche@users.noreply.github.com>
2023-10-28 21:23:02 -0300

    Added GithubContext using Context API and useReducer

--- github-user-search/src/App.js
+++ github-user-search/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header";
 import Container from "react-bootstrap/Container";
 import Search from "./components/Search";
@@ -9,48 +9,14 @@ import Paginator from "./components/Paginator";
 import UserRepos from "./components/UserRepos";
 import Footer from "./components/Footer";
 
-import {
-  getUserDetails,
-  getAllUserRepositories,
-} from "./services/githubService";
+import { useGithubContext } from "./context/GithubContext";
 
 import "./App.css";
 
 function App() {
-  // Inicialização dos estados
-  const [userData, setUserData] = useState(null);
-  const [userRepos, setUserRepos] = useState([]);
-  const [error, setError] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [searchPerformed, setSearchPerformed] = useState(false);
-  const itemsPerPage = 10;
-
-  // Função para lidar com a pesquisa de usuário
-  const handleSearch = async (username) => {
-    try {
-      setIsLoading(true);
-      const userDetails = await getUserDetails(username);
-      const userRepos = await getAllUserRepositories(username);
-      setUserData(userDetails);
-      setUserRepos(userRepos);
-      setError(null);
-      setSearchPerformed(true);
-    } catch (error) {
-      setUserData(null);
-      setUserRepos([]);
-      setError("User not found.");
-      setSearchPerformed(false);
-    } finally {
-      setIsLoading(false);
-      setCurrentPage(1);
-    }
-  };
-
-  // Função para lidar com a mudança de página
-  const handlePageChange = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
+  // Usando o contexto para acessar o estado e funções
+  const { state, handleSearch, handlePageChange } = useGithubContext();
+  const { userData, userRepos, error, isLoading,  currentPage, searchPerformed, itemsPerPage } = state;
 
   // Calcular o índice do primeiro item na página atual
   const startIndex = (currentPage - 1) * itemsPerPage;
---
+++ github-user-search/src/context/GithubContext.js
@@ -0,0 +1,94 @@
+import React, { createContext, useReducer, useContext } from "react";
+import {
+  getUserDetails,
+  getAllUserRepositories,
+} from "../services/githubService";
+
+// Estrutura inicial do estado
+const initialState = {
+  userData: null,
+  userRepos: [],
+  error: null,
+  isLoading: false,
+  currentPage: 1,
+  searchPerformed: false,
+  itemsPerPage: 10,
+};
+
+// Criação do contexto
+const GithubContext = createContext();
+
+// Função de redução para atualizar o estado com base em ações
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_USER_DATA":
+      return { ...state, userData: action.payload };
+    case "SET_USER_REPOS":
+      return { ...state, userRepos: action.payload };
+    case "SET_ERROR":
+      return { ...state, error: action.payload };
+    case "SET_IS_LOADING":
+      return { ...state, isLoading: action.payload };
+    case "SET_CURRENT_PAGE":
+      return { ...state, currentPage: action.payload };
+    case "SET_SEARCH_PERFORMED":
+      return { ...state, searchPerformed: action.payload };
+    default:
+      return state;
+  }
+};
+
+// Provedor do contexto que fornece o estado e funções de atualização
+export function GithubProvider({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  // Função para lidar com a pesquisa de usuário
+  const searchGithubByUserWithContext = async (username) => {
+    try {
+      dispatch({ type: "SET_IS_LOADING", payload: true });
+      const userDetails = await getUserDetails(username);
+      dispatch({ type: "SET_USER_DATA", payload: userDetails });
+      const userRepos = await getAllUserRepositories(username);
+      dispatch({ type: "SET_USER_REPOS", payload: userRepos });
+      dispatch({ type: "SET_ERROR", payload: null });
+      dispatch({ type: "SET_SEARCH_PERFORMED", payload: true });
+    } catch (error) {
+      dispatch({ type: "SET_USER_DATA", payload: null });
+      dispatch({ type: "SET_USER_REPOS", payload: [] });
+      dispatch({ type: "SET_ERROR", payload: "User not found." });
+      dispatch({ type: "SET_SEARCH_PERFORMED", payload: false });
+    } finally {
+      dispatch({ type: "SET_IS_LOADING", payload: false });
+      dispatch({ type: "SET_CURRENT_PAGE", payload: 1 });
+    }
+  };
+
+  // Função para lidar com a mudança de página
+  const handlePageChange = (pageNumber) => {
+    dispatch({ type: "SET_CURRENT_PAGE", payload: pageNumber });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{
+        state,
+        handleSearch: searchGithubByUserWithContext,
+        handlePageChange,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+}
+
+// Hook personalizado para usar o contexto
+export function useGithubContext() {
+    const context = useContext(GithubContext);
+    if (!context) {
+      throw new Error(
+        "useGithubContext must be used within a GithubProvider"
+      );
+    }
+    return context;
+  }
+  
